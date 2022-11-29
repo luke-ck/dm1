da3bf2af6d3a05a7fc20ab5c1d5cf2dc102fe2fa
Lucas Cosier <9642742+luke-ck@users.noreply.github.com>
2022-11-29 18:51:54 +0100

    finish exercise 2 hw 4

--- homework 4/code/ex2.py
+++ homework 4/code/ex2.py
@@ -6,11 +6,12 @@ Part 2: Decision Trees
 
 Authors: Anja Gumpinger, Bastian Rieck
 '''
-from math import floor, ceil
-
 import numpy as np
 from sklearn.datasets import load_iris
-
+from sklearn.model_selection import KFold
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.base import clone
 
 def compute_information_content(y):
     '''
@@ -23,7 +24,7 @@ def compute_information_content(y):
     classes = np.unique(y)
     info_content = 0
     for c in classes:
-        p = len(y[y == c]) / len(y)
+        p = len(y[y == c]) / len(y)  # this is just the frequency
         info_content -= p * np.log2(p)
 
     return info_content
@@ -36,12 +37,10 @@ def split_data(X, y, attribute_index, theta):
     :param X: feature matrix
     :param y: class labels
     :param attribute_index: index of the attribute to split on, ranges between 0 and 3
-    :param theta: threshold to split on
+    :param theta: thetaold to split on
     :return:
         two subsets of X and y
     """
-    # split based on the attribute_index and theta
-    # return the two subsets of X and y
 
     y_left = y[X[:, attribute_index] < theta]
     y_right = y[X[:, attribute_index] >= theta]
@@ -57,7 +56,7 @@ def compute_information_gain(X, y, attribute_index, theta):
     :param X: feature matrix
     :param y: class labels
     :param attribute_index: index of the attribute to split on
-    :param theta: threshold to split on
+    :param theta: thetaold to split on
     :return: information gain
     '''
 
@@ -72,63 +71,52 @@ def compute_information_gain(X, y, attribute_index, theta):
     return info_gain
 
 
-def find_best_split(X, y):
-    '''
-    Finds the best split for a dataset X with labels y.
-
-    :param X: feature matrix
-    :param y: class labels
-    :return: best split (attribute_index, theta)
-    '''
-
-    # find the best split for the dataset X with labels y
-    # return the best split (attribute_index, theta)
-    best_gain = 0
-    best_split = (0, 0)
-    for i in range(X.shape[1]):
-        for theta in np.unique(X[:, i]):
-            info_gain = compute_information_gain(X, y, i, theta)
-            if info_gain > best_gain:
-                best_gain = info_gain
-                best_split = (i, theta)
-
-    return best_split
-
-
 def cross_validation(X, y, k, model):
     """
-    function that performs k-fold cross validation on the dataset X with labels y
+    function that performs k-fold cross validation for a model on the dataset X with labels y
     :param X: Feature matrix
     :param y: class labels
     :param k: number of folds
+    :param model: model to evaluate
     :return:
-        mean accuracy of the k-fold cross validation
+        mean accuracy
+        feature scores with indices of the features sorted by importance
     """
 
-    kf = KFold(n_splits=k, shuffle=True, random_state=42)
+    kf = KFold(n_splits=k, shuffle=True)
     accuracies = []
+    feature_scores = []
+
     for train_index, test_index in kf.split(X, y):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
+        model = clone(model)  # make sure to train on a fresh model
+
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
         accuracies.append(accuracy_score(y_test, y_pred))
+        feature_scores.append(model.feature_importances_)
 
-    return np.mean(accuracies)
+    # now we compute the mean over the folds and sort
+    accuracies = np.mean(accuracies)
+    mean_feature_scores = np.mean(feature_scores, axis=0)
+    feature_importances = np.argsort(mean_feature_scores)[::-1]
+    feature_importances = list(zip(feature_importances, mean_feature_scores[feature_importances]))
+
+    return accuracies, feature_importances
 
 
-def report_important_features(model, feature_names):
+def report_important_features(feature_names, feature_scores, top_k=2):
     """
     function that prints the most important features of a decision tree
     :param model: decision tree model
     :return:
     """
-    print("Important features:")
-    feature_importances = np.argsort(model.feature_importances_)[::-1]
-    for i, feature in enumerate(feature_importances):
-        print("{}. {} ({})".format(i + 1, feature_names[feature], model.feature_importances_[feature]))
-    print('')
+    for idx, feature in feature_scores[:top_k]:
+        if feature == 0:
+            continue
+        print("{}-({:.3f})".format(feature_names[idx], feature))
 
 
 if __name__ == '__main__':
@@ -150,7 +138,7 @@ if __name__ == '__main__':
     attributes_to_try = [0, 1, 2, 3]
     theta_to_try = [5.5, 3.0, 2.0, 1.0]
     print('Exercise 2.b')
-    print('------------')
+    print('-' * 12)
     for i, theta in zip(attributes_to_try, theta_to_try):
         information_gain = compute_information_gain(X, y, i, theta)
         print('split on attribute {0}, threshold {1:.1f}: Information gain = {2:.2f}'.format(
@@ -159,22 +147,21 @@ if __name__ == '__main__':
     print('')
 
     print('Exercise 2.c')
-    print('I would choose to split on attribute {0}, threshold {1:.1f} since it has the ' \
-          'highest information gain.'.format(feature_names[2], 2.0))
+    print('-' * 12)
+    print('I would choose to split on attribute {0} < {2:.1f} or attribute {1} '
+          '< {3:.1f} since they have the highest information gain of 0.92.'
+          .format(feature_names[2], feature_names[3], theta_to_try[2], theta_to_try[3]))
 
     print('')
 
     ####################################################################
     # Exercise 2.d
     ####################################################################
-
+    print('Exercise 2.d')
+    print('-' * 12)
     # Do _not_ remove this line because you will get different splits
     # which make your results different from the expected ones...
     np.random.seed(42)
-
-    from sklearn.model_selection import KFold
-    from sklearn.tree import DecisionTreeClassifier
-    from sklearn.metrics import accuracy_score
 
     # split the data into k folds
     k = 5
@@ -182,24 +169,28 @@ if __name__ == '__main__':
 
     # compute the accuracy of the classifier on the test set
     # for each fold
-    accuracy = cross_validation(X, y, k, clf)
+    accuracy, feature_scores = cross_validation(X, y, k, clf)
 
     print('Accuracy score using cross-validation')
-    print('-------------------------------------\n')
+    print('-' * 12)
     print('Mean accuracy: {0:.2f}'.format(accuracy * 100))
 
     print('')
     print('Feature importances for _original_ data set')
-    print('-------------------------------------------\n')
-    report_important_features(clf, feature_names)
-
+    print('-' * 12)
+    report_important_features(feature_names, feature_scores)
     # remove the label 2
     X = X[y != 2]
     y = y[y != 2]
 
     clf = DecisionTreeClassifier()
-    accuracy = cross_validation(X, y, k, clf)
+    accuracy, feature_scores = cross_validation(X, y, k, clf)
+
+    print('')
     print('Feature importances for _reduced_ data set')
-    print('Mean accuracy: {0:.2f}'.format(accuracy * 100))
-    print('------------------------------------------\n')
-    report_important_features(clf, feature_names)
+    # print('Mean accuracy: {0:.2f}'.format(accuracy * 100))
+    print('-' * 12)
+    report_important_features(feature_names, feature_scores)
+    print('')
+    print('Feature importance is calculated as a measure of the weighted decrease in node impurity.'
+          ' The feature score of 1 means that splitting on one feature is enough to classify the (binary) data.')
