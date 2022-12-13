faa73590589928b4946c07053631573cca55bd98
Lucas Cosier <9642742+luke-ck@users.noreply.github.com>
2022-12-13 14:46:36 +0100

    finish exercise 3c

--- homework 5/code/string-kernels.py
+++ homework 5/code/string-kernels.py
@@ -1,21 +1,44 @@
 import numpy as np
 
+k = 3
+
+
+def dirac_kernel(X, Y):
+    if X == Y:
+        return 1
+    else:
+        return 0
+
 
 def compute_kbase_kernel(S, T):
-    if S[0] == 'G' or T[0] == 'G':
+    assert len(S) == k
+    assert len(T) == k
+
+    if S[0] != 'G' or T[0] != 'G':
         return 0
     else:
-        return np.sum([1 for i in range(len(S)) if S[i] == T[i]])
+        return np.sum([dirac_kernel(S[i], T[i]) for i in range(len(S))])
 
 
-def compute_rkernel(X, Y):
-    # kbase(s, s′) = {0 if s_0 = G or s′_0 = G else ∑_{i = 0}^2 k(s_i, s′_i)
+def compute_rkernel(X: str, Y: str, k=3) -> float:
+    # kbase(s, s′) = {0 if s_0 != G or s′_0 != G else ∑_{i = 0}^2 k(s_i, s′_i)
     kbase = 0
-    for i in range(len(X) - 2):
-        for j in range(len(Y) - 2):
-            kbase += compute_kbase_kernel(X[i:i + 3], Y[j:j + 3])
-            # if kbase != 0:
-            #     print(X[i:i + 3], Y[j:j + 3], kbase)
+    X_kmers = []
+    Y_kmers = []
+    for i in range(len(X) - k + 1):
+        for j in range(len(Y) - k + 1):
+            X_kmer = X[i:i + k]
+            Y_kmer = Y[j:j + k]
+
+            if X_kmer not in X_kmers:
+                X_kmers.append(X_kmer)
+            if Y_kmer not in Y_kmers:
+                Y_kmers.append(Y_kmer)
+
+            kbase += compute_kbase_kernel(X_kmer, Y_kmer)
+
+    print(f" string {X.__repr__()} k_mers => {X_kmers}")
+    print(f" string {Y.__repr__()} k_mers => {Y_kmers}")
 
     return kbase
 
@@ -25,7 +48,6 @@ if __name__ == '__main__':
     X1 = 'GPAGFAGPPGDA'
     X2 = 'PRGDQGPVGRTG'
     X3 = 'GFPNFDVSVSDM'
-
     # Compute the kernel
-    print(compute_rkernel(X1, X2))
-    print(compute_rkernel(X1, X3))
+    print(f"k(X1,X2)={compute_rkernel(X1, X2, k)}")
+    print(f"k(X1,X3)={compute_rkernel(X1, X3, k)}")
